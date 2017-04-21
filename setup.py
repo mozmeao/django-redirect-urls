874694a89e50de3c7073e52272c209a93d8dd3c3
Paul McLanahan <pmac@mozilla.com>
2017-04-21 15:30:21 -0400

    Final tweaks for initial release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
